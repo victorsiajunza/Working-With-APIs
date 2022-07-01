4af75cf56ac5982885ea1e9152b683af3bf2e7c0
victorsiajunza <logzee26@gmail.com>
2022-07-02 03:09:57 +1000

    still making models

--- I4G029762GRT/links/models.py
+++ I4G029762GRT/links/models.py
@@ -1,26 +1,18 @@
-from os import link
+
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
+
 # Create your models here.
-class links(models.Model):
- link
+class Link(models.Model):
 
-
-target_url : models.URLField(max_length=200)
-
- 
-
+  target_url : models.URLField(max_length=200)
 description : models.CharField(max_length=200)
-
- 
-
-identifier: models.SlugField(max_length=20 blank=True,unique=True)
-
- 
-
+identifier: models.SlugField(max_length=20,blank=True,unique=True)
 author : models.ForeignKey(get_user_model())
+created_date : models.DateTimeField(auto_now=True)
+active : models.BooleanField(default=True)
 
- 
-
-created_date : A date-time column, use Django’s models.DateTimeField.
+def __str__(self):
+        return self.title
