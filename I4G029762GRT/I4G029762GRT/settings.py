304ca9f620a452e86e579bb1c12ba29809c9f9a7
victorsiajunza <logzee26@gmail.com>
2022-07-02 15:05:41 +1000

    made migrations

--- I4G029762GRT/I4G029762GRT/settings.py
+++ I4G029762GRT/I4G029762GRT/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'links'
-    'rest_framework'
+    'links',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- I4G029762GRT/links/models.py
+++ I4G029762GRT/links/models.py
@@ -8,11 +8,10 @@ from django.contrib.auth import get_user_model
 class Link(models.Model):
 
   target_url : models.URLField(max_length=200)
-description : models.CharField(max_length=200)
-identifier: models.SlugField(max_length=20,blank=True,unique=True)
-author : models.ForeignKey(get_user_model())
-created_date : models.DateTimeField(auto_now=True)
-active : models.BooleanField(default=True)
+  description : models.CharField(max_length=200)
+  identifier: models.SlugField(max_length=20,blank=True,unique=True)
+  author : models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  created_date : models.DateTimeField(auto_now=True)
+  active : models.BooleanField(default=True)
 
-def __str__(self):
-        return self.title
+
