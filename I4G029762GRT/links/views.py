9bd8000e4a6ef33cd1ea2ab1a17eb2a034a469a4
victorsiajunza <logzee26@gmail.com>
2022-07-04 20:11:27 +1000

    latest update

--- I4G029762GRT/links/views.py
+++ I4G029762GRT/links/views.py
@@ -1,3 +1,29 @@
+from msilib.schema import ListView
+from urllib import response
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from links.models import Link
+from .serializers import LinkSerializer
 # Create your views here.
+class PostListApi():
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+    
+
+class PostCreateApi():
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetailApi():
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi():
+    queryset = Link.object.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi():
+    queryset= Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
