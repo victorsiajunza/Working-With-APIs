4ab00177e1be38002fce9507fb28eb32513b5cb4
victorsiajunza <logzee26@gmail.com>
2022-07-04 22:25:46 +1000

    made changes

--- I4G029762GRT/I4G029762GRT/settings.py
+++ I4G029762GRT/I4G029762GRT/settings.py
@@ -50,7 +50,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 ROOT_URLCONF = 'I4G029762GRT.urls'
 
 TEMPLATES = [
--- I4G029762GRT/links/views.py
+++ I4G029762GRT/links/views.py
@@ -1,28 +1,28 @@
-from msilib.schema import ListView
+
 from urllib import response
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import generics
 from links.models import Link
 from .serializers import LinkSerializer
 # Create your views here.
-class PostListApi():
+class PostListApi(generics.ListAPIView):
     queryset = Link.objects.filter(active=True)
     serializer_class = LinkSerializer
     
 
-class PostCreateApi():
+class PostCreateApi(generics.CreateAPIView):
     queryset = Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
-class PostDetailApi():
+class PostDetailApi(generics.RetrieveAPIView):
     queryset = Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
-class PostUpdateApi():
+class PostUpdateApi(generics.UpdateAPIView):
     queryset = Link.object.filter(active=True)
     serializer_class = LinkSerializer
 
-class PostDeleteApi():
+class PostDeleteApi(generics.DestroyAPIView):
     queryset= Link.objects.filter(active=True)
     serializer_class = LinkSerializer
 
